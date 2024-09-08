5ac8c64330a7cc1e93b07f878cda46bc4f5f13ea
TErben-cen55497 <terben@csas.cz>
2024-09-08 19:48:08 +0200

    .

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+const campuses = {
+  travel: "/01GGDHJAQMA1D0VMK8WV22BJJN/01HTYZPR5T7REDAGHN618DQBN0",
+  gm: "/01GGDHJAQMA1D0VMK8WV22BJJN/01HK2B2DWW42VSZ4TZ59QSP7T2",
+};
+
+const browser = await puppeteer.launch({ headless: false });
+const page = await browser.newPage();
+
+const login = async () => {
   await page.goto("https://app.jointherealworld.com/auth/login", {
     waitUntil: "networkidle0",
   });
@@ -17,20 +23,12 @@ dotenv.config();
     button?.click();
   });
   await page.waitForNetworkIdle();
-  await page.goto("https://app.jointherealworld.com/chat/01GGDHJAQMA1D0VMK8WV22BJJN/01HTYZPR5T7REDAGHN618DQBN0");
+};
+
+const navigateToCampus = async (campus) => {
+  await page.goto(`https://app.jointherealworld.com/chat${campuses[campus]}`);
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const city = await getRandomCity();
-  const url = await getImage(city);
-  const image = await (await fetch(url)).arrayBuffer();
-  const imagePath = `/Users/cen55497/trw/cities/${city}_${new Date().toISOString()}.jpeg`;
-  fs.writeFileSync(imagePath, Buffer.from(image));
-  const inputFileSelector = 'input[type="file"]';
-  await page.waitForSelector(inputFileSelector);
-  const inputElement = await page.$(inputFileSelector);
-  await inputElement.uploadFile(imagePath);
-  await page.type("#chat-input", city);
-  await page.keyboard.press("Enter");
-})();
+};
 
 const getImage = async (city) => {
   const res = await (
@@ -50,6 +48,34 @@ const getRandomCity = async () => {
     .toString()
     .split("\n")
     .map((e) => e.split(",")[0].replaceAll('"', ""));
-  console.log(2222);
   return cities[_.random(cities.length - 1)];
 };
+
+const postTravelPic = async () => {
+  await navigateToCampus("travel");
+  const city = await getRandomCity();
+  const url = await getImage(city);
+  const image = await (await fetch(url)).arrayBuffer();
+  const imagePath = `/Users/cen55497/trw/cities/${city}_${new Date().toISOString()}.jpeg`;
+  fs.writeFileSync(imagePath, Buffer.from(image));
+  const inputFileSelector = 'input[type="file"]';
+  await page.waitForSelector(inputFileSelector);
+  const inputElement = await page.$(inputFileSelector);
+  await inputElement.uploadFile(imagePath);
+  await page.type("#chat-input", city);
+  //   await page.keyboard.press("Enter");
+};
+
+const sayGM = async () => {
+  await navigateToCampus("gm");
+  await page.type("#chat-input", "GM");
+  //   await page.keyboard.press("Enter");
+};
+
+const main = async () => {
+  await login();
+  await postTravelPic();
+  await sayGM();
+};
+
+await main();
